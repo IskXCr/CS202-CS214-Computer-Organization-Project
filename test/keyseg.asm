3b64c30a19daa557b3c9fa2afc29423aa52d9873
cly1231 <90187258+cly1231@users.noreply.github.com>
2023-05-25 15:47:22 +0800

    IO_test

--- test/keyseg.asm
+++ test/keyseg.asm
@@ -3,13 +3,18 @@ buf: .word 0x0000
 .text 0x0000
 
 start: 
-lui   $1,0xffff		
-ori   $28,$1,0x0000
+lui   $28,0xffff		
+ori   $28,$28,0x0000
 
 switled:								
-	lw   $1,0x10($28)		# right 16 switch
-	sw   $1,0x2C($28)		# right 16 LED
-	lw   $1,0x0C($28)		# left 8 switch
-	sw   $1,0x24($28)		# left 8 LED
-	addi $1,$1, 0
+	lw $1, 0x04($28)  # sw[23] check the case of scenario
+	sw $1, 0x20($28)  # LED[19]
+	lw $1, 0x08($28)  # sw[22:20] testcase
+	sw $1, 0x30($28)  # LED Tube LEFT
+	lw $1, 0x0c($28)  # sw[15:8]
+	sw $1, 0x30($28)  # LED Tube LEFT
+	lw $1, 0x10($28)  # sw[7:0]
+	sw $1, 0x34($28)  # LED Tube RIGHT
+	lw $1, 0x14($28)  # keypad
+	sw $1, 0x38($28)  # LED[15:0]
 	j switled
