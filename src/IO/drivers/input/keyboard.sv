09483ad70f23e7cfd533ffd3e184c26f81023630
Sparky <48359461+Sparky579@users.noreply.github.com>
2023-05-29 14:33:46 +0800

    Update keyboard.sv

--- src/IO/drivers/input/keyboard.sv
+++ src/IO/drivers/input/keyboard.sv
@@ -12,7 +12,7 @@ module keyboard (
   wire       key_clk;
   reg [7:0] state;
   reg start;
-  reg idle;
+  reg [3:0] idle;
 
   always @(posedge clk or posedge rst) begin
     if (rst)
@@ -26,6 +26,7 @@ module keyboard (
   always @(posedge key_clk or posedge rst) begin
     if (rst) begin
         col <= 4'b0001;
+        idle <= 4'b1111;
     end
     else if (col == 4'b1000) col <= 4'b0001;
     else if (col == 4'b0000) col <= 4'b0001;
@@ -39,33 +40,32 @@ module keyboard (
     end else begin
                   if (row != 4'hF) begin
                     case ({col, row})
-                      {4'b0001, 4'b1110}: keyboard_val <= 4'b0001;//1
-                                          {4'b0001, 4'b1101}: keyboard_val <= 4'b0100;//4
-                                          {4'b0001, 4'b1011}: keyboard_val <= 4'b0111;//7
-                                          {4'b0001, 4'b0111}: keyboard_val <= 4'b1110;//*
-                    
-                                          {4'b0010, 4'b1110}: keyboard_val <= 4'b0010;//2
-                                          {4'b0010, 4'b1101}: keyboard_val <= 4'b0101;//5
-                                          {4'b0010, 4'b1011}: keyboard_val <= 4'b1000;//8
-                                          {4'b0010, 4'b0111}: keyboard_val <= 4'b0000;//0
-                    
-                                          {4'b0100, 4'b1110}: keyboard_val <= 4'b0011;//3
-                                          {4'b0100, 4'b1101}: keyboard_val <= 4'b0110;//6
-                                          {4'b0100, 4'b1011}: keyboard_val <= 4'b1001;//9
-                                          {4'b0100, 4'b0111}: keyboard_val <= 4'b1111;//#
-                    
-                                          {4'b1000, 4'b1110}: keyboard_val <= 4'b1010;//A
-                                          {4'b1000, 4'b1101}: keyboard_val <= 4'b1011;//B
-                                          {4'b1000, 4'b1011}: keyboard_val <= 4'b1100;//C
-                                          {4'b1000, 4'b0111}: keyboard_val <= 4'b1101;//D
+                       {4'b0001, 4'b1110}: keyboard_val <= 4'b0001;//1
+                          {4'b0001, 4'b1101}: keyboard_val <= 4'b0100;//4
+                                {4'b0001, 4'b1011}: keyboard_val <= 4'b0111;//7
+                                    {4'b0001, 4'b0111}: keyboard_val <= 4'b1110;//*               
+                                                             {4'b0010, 4'b1110}: keyboard_val <= 4'b0010;//2
+                                                             {4'b0010, 4'b1101}: keyboard_val <= 4'b0101;//5
+                                                             {4'b0010, 4'b1011}: keyboard_val <= 4'b1000;//8
+                                                             {4'b0010, 4'b0111}: keyboard_val <= 4'b0000;//0
+                                       
+                                                             {4'b0100, 4'b1110}: keyboard_val <= 4'b0011;//3
+                                                             {4'b0100, 4'b1101}: keyboard_val <= 4'b0110;//6
+                                                             {4'b0100, 4'b1011}: keyboard_val <= 4'b1001;//9
+                                                             {4'b0100, 4'b0111}: keyboard_val <= 4'b1111;//#
+                                       
+                                                             {4'b1000, 4'b1110}: keyboard_val <= 4'b1010;//A
+                                                             {4'b1000, 4'b1101}: keyboard_val <= 4'b1011;//B
+                                                             {4'b1000, 4'b1011}: keyboard_val <= 4'b1100;//C
+                                                             {4'b1000, 4'b0111}: keyboard_val <= 4'b1101;//D
                       default: keyboard_val <= 4'b1111;
                     endcase
                     press <= 1;
-                    idle <= 0;
+                    idle[col] <= 0;
                   end else begin
                     keyboard_val <= 4'b0000;
                     press <= 0;
-                    idle <= 1;
+                    idle[col] <= 1;
                   end
     end
   end
@@ -75,7 +75,7 @@ module keyboard (
         start <= 0;
     end
     else begin
-        if (idle == 1) state <= state;
+        if (idle[col] == 1) state <= state;
         else begin
             case (keyboard_val)
                 4'b1111: begin 
