55fad33e5ecd4d64c2d120e23ce7e710bbe43911
Sparky <48359461+Sparky579@users.noreply.github.com>
2023-05-29 11:51:12 +0800

    Update keyboard.sv

--- src/IO/drivers/input/keyboard.sv
+++ src/IO/drivers/input/keyboard.sv
@@ -24,7 +24,7 @@ module keyboard (
   input           clk,
   input           rst,
   input      [3:0] row,
-  output reg [2:0] col,
+  output reg [3:0] col,
   output reg [3:0] keyboard_val,
   output reg      press,
   output reg [7:0] wdata
@@ -46,10 +46,11 @@ module keyboard (
   
   always @(posedge key_clk or posedge rst) begin
     if (rst) begin
-        col <= 3'b000;
+        col <= 4'b0001;
     end
-    else if (col == 3'b100) col <= 3'b000;
-    else col <= col + 1'b1;
+    else if (col == 4'b1000) col <= 4'b0001;
+    else if (col == 4'b0000) col <= 4'b0001;
+    else col <= col << 1;
   end
 
   always @(posedge key_clk or posedge rst) begin
@@ -59,25 +60,25 @@ module keyboard (
     end else begin
                   if (row != 4'hF) begin
                     case ({col, row})
-                      {3'b001, 4'b1110}: keyboard_val <= 4'b0001;//1
-                      {3'b001, 4'b1101}: keyboard_val <= 4'b0100;//4
-                      {3'b001, 4'b1011}: keyboard_val <= 4'b0111;//7
-                      {3'b001, 4'b0111}: keyboard_val <= 4'b1110;//*
+                      {4'b0001, 4'b1110}: keyboard_val <= 4'b0001;//1
+                      {4'b0001, 4'b1101}: keyboard_val <= 4'b0100;//4
+                      {4'b0001, 4'b1011}: keyboard_val <= 4'b0111;//7
+                      {4'b0001, 4'b0111}: keyboard_val <= 4'b1110;//*
 
-                      {3'b010, 4'b1110}: keyboard_val <= 4'b0010;//2
-                      {3'b010, 4'b1101}: keyboard_val <= 4'b0101;//5
-                      {3'b010, 4'b1011}: keyboard_val <= 4'b1000;//8
-                      {3'b010, 4'b0111}: keyboard_val <= 4'b0000;//0
+                      {4'b0010, 4'b1110}: keyboard_val <= 4'b0010;//2
+                      {4'b0010, 4'b1101}: keyboard_val <= 4'b0101;//5
+                      {4'b0010, 4'b1011}: keyboard_val <= 4'b1000;//8
+                      {4'b0010, 4'b0111}: keyboard_val <= 4'b0000;//0
 
-                      {3'b011, 4'b1110}: keyboard_val <= 4'b0011;//3
-                      {3'b011, 4'b1101}: keyboard_val <= 4'b0110;//6
-                      {3'b011, 4'b1011}: keyboard_val <= 4'b1001;//9
-                      {3'b011, 4'b0111}: keyboard_val <= 4'b1111;//#
+                      {4'b0100, 4'b1110}: keyboard_val <= 4'b0011;//3
+                      {4'b0100, 4'b1101}: keyboard_val <= 4'b0110;//6
+                      {4'b0100, 4'b1011}: keyboard_val <= 4'b1001;//9
+                      {4'b0100, 4'b0111}: keyboard_val <= 4'b1111;//#
 
-                      {3'b100, 4'b1110}: keyboard_val <= 4'b1010;//A
-                      {3'b100, 4'b1101}: keyboard_val <= 4'b1011;//B
-                      {3'b100, 4'b1011}: keyboard_val <= 4'b1100;//C
-                      {3'b100, 4'b0111}: keyboard_val <= 4'b1101;//D
+                      {4'b1000, 4'b1110}: keyboard_val <= 4'b1010;//A
+                      {4'b1000, 4'b1101}: keyboard_val <= 4'b1011;//B
+                      {4'b1000, 4'b1011}: keyboard_val <= 4'b1100;//C
+                      {4'b1000, 4'b0111}: keyboard_val <= 4'b1101;//D
                       default: keyboard_val <= 4'b1111;
                     endcase
                     press <= 1;
@@ -116,3 +117,4 @@ module keyboard (
     else wdata <= state;
   end
 endmodule
+
